44436581aaa94d8790c2fc2d171b6a279dd4c65c
XiaobingSuper <xiaobing.zhang@intel.com>
2021-12-02 10:03:29 +0000

    init openNMT code

---
+++ test_xianf_quant.sh
@@ -0,0 +1,21 @@
+#export LD_LIBRARY_PATH="/home/ubuntu/":$LD_LIBRARY_PATH
+export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
+export OMP_NUM_THREADS=4
+export KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
+export KMP_BLOCKTIME=1
+
+#taskset -c 0-20 python3 /home/ubuntu/opennmt/OpenNMT-py-1.2.0/translate.py \
+taskset -c 4-7 python3 /home/xiaobing/Download/tencent_cloud/opennmt/OpenNMT-py-1.2.0_new/translate.py \
+    --model ./checkpoint_step_250000.pt \
+    --src ./test_data/test.en \
+    --output ./translated_xianf \
+    --shard_size 1000000 \
+    --beam_size 4 \
+    --max_length 200 \
+    --length_penalty "wu" \
+    --alpha 0.6 \
+    --coverage_penalty "wu" \
+    --beta 0.6 \
+    --batch_size 1 \
+    --report_time \
+    --fp32
