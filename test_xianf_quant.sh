1a19c5ed1a4a6bb86196db89fbdecfc56132df7f
zhang, xiaobing <xiaobing.zhang@intel.com>
2022-04-20 22:30:51 +0800

    updata test_xianf_quant.sh

--- test_xianf_quant.sh
+++ test_xianf_quant.sh
@@ -1,11 +1,11 @@
 #export LD_LIBRARY_PATH="/home/ubuntu/":$LD_LIBRARY_PATH
-export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
+export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:-1,muzzy_decay_ms:-1"
 export OMP_NUM_THREADS=4
 export KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
 export KMP_BLOCKTIME=1
 
 #taskset -c 0-20 python3 /home/ubuntu/opennmt/OpenNMT-py-1.2.0/translate.py \
-numactl --physcpubind=0-3 --membind=0 python  /home/xiaobing/Downloads/openNMT/OpenNMT-py-1.2.0_new/translate.py \
+numactl --physcpubind=0-3 --membind=0 python OpenNMT-py-1.2.0_new/translate.py \
     --model ./checkpoint_step_250000.pt \
     --src ./test_data/test.en \
     --output ./translated_xianf \
